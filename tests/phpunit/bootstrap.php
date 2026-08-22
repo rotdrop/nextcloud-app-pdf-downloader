fc098858bab9be351afbc81a4c178eddc0de834b
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-08-22 18:45:56 +0200

    phpunit, polish empty test-suite. Ouch.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,7 +1,37 @@
 <?php
+/**
+ * Recursive PDF Downloader App for Nextcloud
+ *
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @copyright 2022, 2023, 2024, 2025, 2026 Claus-Justus Heine <himself@claus-justus-heine.de>
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *"
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+namespace OCA\PdfDownloader\Tests;
+
+date_default_timezone_set('UTC');
+
+putenv('TEST_DONT_LOAD_APPS=1');
+$oldReporting = error_reporting(0);
 require_once __DIR__ . '/../../../../tests/bootstrap.php';
+error_reporting($oldReporting);
+
+require_once __DIR__ . "/../../vendor/autoload.php";
 
 $infoXml = new \SimpleXMLElement(file_get_contents(__DIR__ . '/../../appinfo/info.xml'));
 $app = (string)$infoXml->id;
-\OC_App::registerAutoloading($app, __DIR__ . '/../..');
+
+// \OC_App::registerAutoloading($app, __DIR__ . '/../..');
